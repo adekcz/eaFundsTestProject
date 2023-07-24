ef01a0ae763ec527d8514d698ccc29b370717238
Michal Keda <adekcz@gmail.com>
2023-07-24 13:17:17 +0200

    adding data

---
+++ eafundtest/src/data/data.js
@@ -0,0 +1,602 @@
+export const data = [
+ {
+   "ID": 1247,
+   "First name": "Sofia",
+   "Last name": "Ramos",
+   "Summary": "Evaluating the long-term effects of climate change policies",
+   "Submission date": "2022-11-19",
+   "Payout amount": "$200,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -4.9
+ },
+ {
+   "ID": 1467,
+   "First name": "Noah",
+   "Last name": "Ali",
+   "Summary": "Improving the welfare of working animals in developing countries",
+   "Submission date": "2022-04-22",
+   "Payout amount": "$180,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": -4.6
+ },
+ {
+   "ID": 1483,
+   "First name": "Mehmet",
+   "Last name": "Yılmaz",
+   "Summary": "Quantifying long-term risks of AI in financial markets",
+   "Submission date": "2022-12-30",
+   "Payout amount": "$1,000,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 2.6
+ },
+ {
+   "ID": 1783,
+   "First name": "Maria",
+   "Last name": "Gonzalez",
+   "Summary": "Ensuring safety and security in AI deployment",
+   "Submission date": "2022-09-05",
+   "Payout amount": "$1,000,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 0.3
+ },
+ {
+   "ID": 1853,
+   "First name": "James",
+   "Last name": "Lee",
+   "Summary": "Developing resources and materials for Effective Altruism outreach",
+   "Submission date": "2022-08-04",
+   "Payout amount": "$80,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 5
+ },
+ {
+   "ID": 1906,
+   "First name": "Fatima",
+   "Last name": "Ali",
+   "Summary": "Exploring long-term strategies for sustainable development",
+   "Submission date": "2022-07-14",
+   "Payout amount": "$900,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -3.7
+ },
+ {
+   "ID": 2067,
+   "First name": "Sophie",
+   "Last name": "Lefebvre",
+   "Summary": "Strengthening AI alignment frameworks for AGI",
+   "Submission date": "2022-08-06",
+   "Payout amount": "$750,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 3.8
+ },
+ {
+   "ID": 2417,
+   "First name": "Ahmed",
+   "Last name": "Khan",
+   "Summary": "Long-term planning for climate change adaptation",
+   "Submission date": "2022-12-16",
+   "Payout amount": "$350,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -4.3
+ },
+ {
+   "ID": 2508,
+   "First name": "Jacob",
+   "Last name": "Gupta",
+   "Summary": "Promoting the welfare of stray and feral animals in urban areas",
+   "Submission date": "2022-11-14",
+   "Payout amount": "$170,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": -2.3
+ },
+ {
+   "ID": 2679,
+   "First name": "Wei",
+   "Last name": "Chen",
+   "Summary": "Long-term implications of advanced nanotechnology",
+   "Submission date": "2022-09-27",
+   "Payout amount": "$550,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 2.5
+ },
+ {
+   "ID": 2684,
+   "First name": "Sophie",
+   "Last name": "Liu",
+   "Summary": "Promoting the welfare of farm animals through policy advocacy",
+   "Submission date": "2022-09-01",
+   "Payout amount": "$100,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": -2.6
+ },
+ {
+   "ID": 2978,
+   "First name": "Jacob",
+   "Last name": "Lopez",
+   "Summary": "Supporting Effective Altruism projects in developing countries",
+   "Submission date": "2022-06-07",
+   "Payout amount": "$170,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 1.5
+ },
+ {
+   "ID": 3017,
+   "First name": "Olivia",
+   "Last name": "Wilson",
+   "Summary": "Funding research on the impact of emerging technologies within Effective Altruism",
+   "Submission date": "2022-01-12",
+   "Payout amount": "$250,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -1.9
+ },
+ {
+   "ID": 3175,
+   "First name": "Nikolai",
+   "Last name": "Volkov",
+   "Summary": "Strategies for mitigating existential risks of AGI",
+   "Submission date": "2022-11-06",
+   "Payout amount": "$1,200,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 5
+ },
+ {
+   "ID": 3612,
+   "First name": "Sophia",
+   "Last name": "Nguyen",
+   "Summary": "Supporting research on cause prioritization and global catastrophic risks",
+   "Submission date": "2022-07-25",
+   "Payout amount": "$300,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 4.3
+ },
+ {
+   "ID": 3822,
+   "First name": "Olivia",
+   "Last name": "Kumar",
+   "Summary": "Supporting research and advocacy on cause prioritization within Effective Altruism",
+   "Submission date": "2022-12-20",
+   "Payout amount": "$250,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -2.2
+ },
+ {
+   "ID": 3825,
+   "First name": "Sofia",
+   "Last name": "Chowdhury",
+   "Summary": "Evaluating the welfare impact of wildlife rehabilitation and release programs",
+   "Submission date": "2022-10-30",
+   "Payout amount": "$280,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": 1.9
+ },
+ {
+   "ID": 3948,
+   "First name": "Johan",
+   "Last name": "Larsson",
+   "Summary": "Advancing AI safety through robustness testing",
+   "Submission date": "2022-03-18",
+   "Payout amount": "$500,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -1.5
+ },
+ {
+   "ID": 3981,
+   "First name": "Amina",
+   "Last name": "Ibrahim",
+   "Summary": "Analyzing the long-term effects of emerging bioweapons",
+   "Submission date": "2022-06-02",
+   "Payout amount": "$750,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -1.2
+ },
+ {
+   "ID": 4026,
+   "First name": "Lucas",
+   "Last name": "Silva",
+   "Summary": "Analyzing the ethical implications of AI-powered surveillance",
+   "Submission date": "2022-07-30",
+   "Payout amount": "$400,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 4.4
+ },
+ {
+   "ID": 4275,
+   "First name": "Sebastian",
+   "Last name": "Andersen",
+   "Summary": "AI safety audit of critical infrastructure systems",
+   "Submission date": "2022-02-15",
+   "Payout amount": "$350,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 0.4
+ },
+ {
+   "ID": 4320,
+   "First name": "Hiroshi",
+   "Last name": "Tanaka",
+   "Summary": "Assessing risks in AI-assisted healthcare systems",
+   "Submission date": "2022-08-17",
+   "Payout amount": "$400,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 0.7
+ },
+ {
+   "ID": 4639,
+   "First name": "William",
+   "Last name": "Ali",
+   "Summary": "Building a network of Effective Altruism mentors and advisors",
+   "Submission date": "2022-09-11",
+   "Payout amount": "$280,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 3.5
+ },
+ {
+   "ID": 4895,
+   "First name": "Ava",
+   "Last name": "Chen",
+   "Summary": "Investigating the effects of captivity on marine mammal welfare",
+   "Submission date": "2022-03-01",
+   "Payout amount": "$220,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": -1.1
+ },
+ {
+   "ID": 5034,
+   "First name": "Daniel",
+   "Last name": "Chen",
+   "Summary": "Researching the impact of Effective Altruism interventions",
+   "Submission date": "2022-07-04",
+   "Payout amount": "$150,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 3.3
+ },
+ {
+   "ID": 5198,
+   "First name": "Priya",
+   "Last name": "Patil",
+   "Summary": "Enhancing cybersecurity for AI-powered autonomous vehicles",
+   "Submission date": "2022-11-27",
+   "Payout amount": "$250,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -2
+ },
+ {
+   "ID": 5369,
+   "First name": "Isabella",
+   "Last name": "Rossi",
+   "Summary": "Implications of advanced surveillance technologies",
+   "Submission date": "2022-03-07",
+   "Payout amount": "$600,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 3.1
+ },
+ {
+   "ID": 5410,
+   "First name": "Sophie",
+   "Last name": "Gupta",
+   "Summary": "Promoting Effective Altruism principles in policy and governance",
+   "Submission date": "2022-11-11",
+   "Payout amount": "$300,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -4.1
+ },
+ {
+   "ID": 5421,
+   "First name": "Ava",
+   "Last name": "Park",
+   "Summary": "Building collaborations between Effective Altruism and other social impact movements",
+   "Submission date": "2022-04-02",
+   "Payout amount": "$220,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 1.2
+ },
+ {
+   "ID": 5479,
+   "First name": "Liam",
+   "Last name": "Garcia",
+   "Summary": "Evaluating the impact of environmental enrichment on zoo animal welfare",
+   "Submission date": "2022-08-14",
+   "Payout amount": "$250,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": 0.6
+ },
+ {
+   "ID": 5721,
+   "First name": "Ava",
+   "Last name": "Patel",
+   "Summary": "Building a platform for matching individuals with high-impact careers",
+   "Submission date": "2022-01-31",
+   "Payout amount": "$200,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -1.6
+ },
+ {
+   "ID": 5829,
+   "First name": "Ana",
+   "Last name": "Santos",
+   "Summary": "Developing biosecurity measures for synthetic biology",
+   "Submission date": "2022-06-09",
+   "Payout amount": "$250,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 2.2
+ },
+ {
+   "ID": 5872,
+   "First name": "Javier",
+   "Last name": "Lopez",
+   "Summary": "Mitigating AI risks through cooperative game theory",
+   "Submission date": "2022-03-25",
+   "Payout amount": "$400,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 1.4
+ },
+ {
+   "ID": 5904,
+   "First name": "Elena",
+   "Last name": "Kovalenko",
+   "Summary": "Biosecurity measures for next-generation biotech labs",
+   "Submission date": "2022-02-10",
+   "Payout amount": "$300,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 4.6
+ },
+ {
+   "ID": 6152,
+   "First name": "Emma",
+   "Last name": "Johnson",
+   "Summary": "Developing humane alternatives to animal testing",
+   "Submission date": "2022-01-06",
+   "Payout amount": "$200,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": -0.9
+ },
+ {
+   "ID": 6194,
+   "First name": "Mia",
+   "Last name": "López",
+   "Summary": "Assessing the welfare of animals in animal-assisted therapy programs",
+   "Submission date": "2022-06-17",
+   "Payout amount": "$190,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": 4.8
+ },
+ {
+   "ID": 6205,
+   "First name": "Emma",
+   "Last name": "Chen",
+   "Summary": "Promoting diversity and inclusion in the Effective Altruism community",
+   "Submission date": "2022-02-20",
+   "Payout amount": "$150,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -3.5
+ },
+ {
+   "ID": 6589,
+   "First name": "Yuna",
+   "Last name": "Kim",
+   "Summary": "Enhancing resilience against emerging infectious diseases",
+   "Submission date": "2022-04-26",
+   "Payout amount": "$150,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -0.8
+ },
+ {
+   "ID": 6856,
+   "First name": "Chen",
+   "Last name": "Wei",
+   "Summary": "Investigating risks of gene drive technology",
+   "Submission date": "2022-01-16",
+   "Payout amount": "$250,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -0.1
+ },
+ {
+   "ID": 6945,
+   "First name": "Liam",
+   "Last name": "Chowdhury",
+   "Summary": "Funding effective animal advocacy organizations and initiatives",
+   "Submission date": "2022-03-14",
+   "Payout amount": "$220,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 4.7
+ },
+ {
+   "ID": 7018,
+   "First name": "Marta",
+   "Last name": "Lopez",
+   "Summary": "Developing safe and interpretable AI algorithms",
+   "Submission date": "2022-06-29",
+   "Payout amount": "$700,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -1.3
+ },
+ {
+   "ID": 7035,
+   "First name": "Luis",
+   "Last name": "Garcia",
+   "Summary": "Monitoring and mitigating dual-use risks in biotechnology",
+   "Submission date": "2022-04-08",
+   "Payout amount": "$600,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 2.8
+ },
+ {
+   "ID": 7091,
+   "First name": "Ahmed",
+   "Last name": "Mahmoud",
+   "Summary": "Long-term impact assessment of emerging technologies",
+   "Submission date": "2022-10-21",
+   "Payout amount": "$350,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -4.5
+ },
+ {
+   "ID": 7123,
+   "First name": "Isabella",
+   "Last name": "Nguyen",
+   "Summary": "Developing guidelines for ethical wildlife tourism",
+   "Submission date": "2022-12-08",
+   "Payout amount": "$130,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": 2
+ },
+ {
+   "ID": 7191,
+   "First name": "Daniel",
+   "Last name": "Liu",
+   "Summary": "Investigating the effectiveness of long-termist interventions within Effective Altruism",
+   "Submission date": "2022-05-17",
+   "Payout amount": "$200,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 0.5
+ },
+ {
+   "ID": 7196,
+   "First name": "Emily",
+   "Last name": "Wilson",
+   "Summary": "Supporting local Effective Altruism groups and events",
+   "Submission date": "2022-05-24",
+   "Payout amount": "$100,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -4.4
+ },
+ {
+   "ID": 7498,
+   "First name": "Max",
+   "Last name": "Andersen",
+   "Summary": "Investigating the welfare implications of factory farming",
+   "Submission date": "2022-05-29",
+   "Payout amount": "$150,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": 4.9
+ },
+ {
+   "ID": 8063,
+   "First name": "Diego",
+   "Last name": "Fernandez",
+   "Summary": "Biosecurity training and capacity building in developing countries",
+   "Submission date": "2022-08-23",
+   "Payout amount": "$500,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 5
+ },
+ {
+   "ID": 8095,
+   "First name": "Mia",
+   "Last name": "Andersen",
+   "Summary": "Researching the long-term effects of career choice on social impact",
+   "Submission date": "2022-10-13",
+   "Payout amount": "$240,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -0.6
+ },
+ {
+   "ID": 8305,
+   "First name": "Mohammed",
+   "Last name": "Abdullah",
+   "Summary": "Mitigating risks of AI in autonomous vehicles",
+   "Submission date": "2022-05-12",
+   "Payout amount": "$450,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -0.2
+ },
+ {
+   "ID": 8436,
+   "First name": "William",
+   "Last name": "Park",
+   "Summary": "Exploring the welfare implications of invasive species management",
+   "Submission date": "2022-02-23",
+   "Payout amount": "$240,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": -3
+ },
+ {
+   "ID": 8457,
+   "First name": "Sophia",
+   "Last name": "Gonzalez",
+   "Summary": "Promoting Effective Altruism principles in educational institutions",
+   "Submission date": "2022-09-19",
+   "Payout amount": "$120,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -0.7
+ },
+ {
+   "ID": 8791,
+   "First name": "Aisha",
+   "Last name": "Ali",
+   "Summary": "Preventing biosecurity breaches in agricultural biotech",
+   "Submission date": "2022-10-15",
+   "Payout amount": "$150,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -3.2
+ },
+ {
+   "ID": 9024,
+   "First name": "Natsumi",
+   "Last name": "Sato",
+   "Summary": "Ethical considerations in AI use for political decision-making",
+   "Submission date": "2022-10-07",
+   "Payout amount": "$150,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -3.1
+ },
+ {
+   "ID": 9063,
+   "First name": "Olivia",
+   "Last name": "Kumar",
+   "Summary": "Assessing the welfare of animals in research laboratories",
+   "Submission date": "2022-07-10",
+   "Payout amount": "$300,000",
+   "Fund": "Animal Welfare Fund",
+   "Rating": 3.9
+ },
+ {
+   "ID": 9152,
+   "First name": "Ravi",
+   "Last name": "Patel",
+   "Summary": "Investigating potential risks of gene editing technologies",
+   "Submission date": "2022-12-25",
+   "Payout amount": "$100,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": -2.7
+ },
+ {
+   "ID": 9367,
+   "First name": "Noah",
+   "Last name": "Nguyen",
+   "Summary": "Investigating the long-term effects of Effective Altruism on donor behavior",
+   "Submission date": "2022-04-16",
+   "Payout amount": "$180,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": 2.1
+ },
+ {
+   "ID": 9406,
+   "First name": "James",
+   "Last name": "Garcia",
+   "Summary": "Developing effective giving platforms and donation matching programs",
+   "Submission date": "2022-08-29",
+   "Payout amount": "$180,000",
+   "Fund": "Effective Altruism Infrastructure Fund",
+   "Rating": -4.2
+ },
+ {
+   "ID": 9531,
+   "First name": "Rahul",
+   "Last name": "Mukherjee",
+   "Summary": "Exploring strategies for global catastrophic risk reduction",
+   "Submission date": "2022-01-28",
+   "Payout amount": "$800,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 1.7
+ },
+ {
+   "ID": 9610,
+   "First name": "Miyu",
+   "Last name": "Yamamoto",
+   "Summary": "Ensuring ethical AI use in financial systems",
+   "Submission date": "2022-05-05",
+   "Payout amount": "$400,000",
+   "Fund": "Long-Term Future Fund",
+   "Rating": 1.6
+ }
+]
